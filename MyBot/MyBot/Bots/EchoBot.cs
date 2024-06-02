91625482efe7dc32578011a93282e1bd291885b8
qarksangwon <0629psw@gmail.com>
2024-06-02 20:04:05 +0900

    구현 프로젝트

---
+++ MyBot/MyBot/Bots/EchoBot.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        static int startnum = 0;
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Text.Equals("Hello")) //Add by swpark 20230328
+            {
+                var replyText = $"Hello Bot World! \r\n Talk to me and I will repeat it back!";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+                startnum++;
+            }
+            else if(startnum !=0)
+            {
+                var replyText = $"Turn{startnum} : You sent '{turnContext.Activity.Text}'";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+                startnum++;
+            }
+            else
+            {
+                var replyText = $"You want start bot? \r\n You talk to me 'Hello'";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
